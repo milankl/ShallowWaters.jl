a1343228d3a5ce15cced0c07b1885cc83f4a01da
milankl <kloewer@atmlxdt043.atm.ox.ac.uk>
2018-06-28 18:22:36 +0100

    preallocate split for u,v,eta

--- src/preallocate.jl
+++ src/preallocate.jl
@@ -1,17 +1,28 @@
-function preallocate_variables(u,v,η)
+function preallocate_u_vars(u)
 
     # initialise with zeros
     v_u = zeros(u)
-    u_v = zeros(v)
-
     dηdx = zeros(u)
-    dηdy = zeros(v)
-
     dLu = zeros(u)
+
+    return v_u,dηdx,dLu
+end
+
+function preallocate_v_vars(v)
+
+    # initialise with zeros
+    u_v = zeros(v)
+    dηdy = zeros(v)
     dLv = zeros(v)
 
+    return u_v,dηdy,dLv
+end
+
+function preallocate_T_variables(η)
+
+    # initialise with zeros
     dudx = zeros(η)
     dvdy = zeros(η)
 
-    return v_u,u_v,dηdx,dηdy,dLu,dLv,dudx,dvdy
+    return dudx,dvdy
 end
