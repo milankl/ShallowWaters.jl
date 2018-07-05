757a7878a3007233c5d45f31581a5f537748fa76
milankl <kloewer@atmlxdt043.atm.ox.ac.uk>
2018-07-05 13:01:23 +0100

    correct lateral boundary conditions

--- parameters.jl
+++ parameters.jl
@@ -2,7 +2,7 @@
 const Numtype = Float32
 
 const nx = 100                   # number of grid cells in x-direction
-const Lx = 3000e3               # length of the domain in x-direction
+const Lx = 2000e3               # length of the domain in x-direction
 const L_ratio = 1               # Domain aspect ratio of Lx/Ly
 
 const gravity = 10.             # gravitational acceleration
@@ -14,7 +14,7 @@ const Ndays = 100                # number of days to integrate for
 
 # boundary condtions
 const bc_x = "nonperiodic"      # "periodic" or anything else for nonperiodic
-const α = 0.                    # lateral boundary condition parameter
+const α = 2.                    # lateral boundary condition parameter
                                 # 0 free-slip, 0<α<2 partial-slip, 2 no-slip
 
 const c_D = 1e-5                # bottom drag coefficient
