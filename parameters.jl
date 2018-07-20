38d88ecb335c9c5628487e786500567db4dc6eb1
milankl <kloewer@atmlxdt043.atm.ox.ac.uk>
2018-07-20 18:04:44 +0100

    nc output with attributes

--- parameters.jl
+++ parameters.jl
@@ -1,18 +1,18 @@
 # define constants
-#const Numtype = Float32
-const Numtype = Posit{16,0}
+const Numtype = Float32
+#const Numtype = Posit{16,0}
 
-const nx = 200                  # number of grid cells in x-direction
+const nx = 100                  # number of grid cells in x-direction
 const Lx = 2000e3               # length of the domain in x-direction
 const L_ratio = 2               # Domain aspect ratio of Lx/Ly
 
 const gravity = 10.             # gravitational acceleration
 const water_depth = 500.        # layer thickness at rest
-const seamount_heigth = 150.    # heigth of seamount
+const seamount_height = 150.    # height of seamount
 const ρ = 1e3                   # density
 
 const cfl = 0.9                 # CFL number
-const Ndays = 100               # number of days to integrate for
+const Ndays = 10               # number of days to integrate for
 
 # boundary condtions
 const bc_x = "periodic"         # "periodic" or anything else for nonperiodic
@@ -26,7 +26,7 @@ const output = 1                # 1 for nc output 0 for none
 const output_dt = 6             # output time step in hours
 const outpath = "/local/kloewer/julsdata/"
 
-const initial_cond = "ncfile"    # "rest" or "ncfile"
+const initial_cond = "rest"    # "rest" or "ncfile"
 const init_run_id = 0           # only for starting from ncfile
 
 const ϕ = 30.                   # central latitue of the domain
--- src/output.jl
+++ src/output.jl
@@ -6,23 +6,62 @@ function output_ini(u,v,η)
         yvdim = NcDim("y",nvy,values=y_v)
         xTdim = NcDim("x",nx,values=x_T)
         yTdim = NcDim("y",ny,values=y_T)
-        tdim = NcDim("t",nout_total,unlimited=true)
-        #println("NC Dimensions created.")
+        tdim = NcDim("t",0,unlimited=true)
 
         uvar = NcVar("u",[xudim,yudim,tdim],t=Float32)
         vvar = NcVar("v",[xvdim,yvdim,tdim],t=Float32)
         ηvar = NcVar("eta",[xTdim,yTdim,tdim],t=Float32)
-        #println("NC variables created.")
+        tvar = NcVar("t",tdim,t=Int64)
 
-        ncu = NetCDF.create(runpath*"u.nc",uvar,mode=NC_NETCDF4)
-        ncv = NetCDF.create(runpath*"v.nc",vvar,mode=NC_NETCDF4)
-        ncη = NetCDF.create(runpath*"eta.nc",ηvar,mode=NC_NETCDF4)
-        #println("NC ncfiles created.")
+        ncu = NetCDF.create(runpath*"u.nc",[uvar,tvar],mode=NC_NETCDF4)
+        ncv = NetCDF.create(runpath*"v.nc",[vvar,tvar],mode=NC_NETCDF4)
+        ncη = NetCDF.create(runpath*"eta.nc",[ηvar,tvar],mode=NC_NETCDF4)
 
         NetCDF.putvar(ncu,"u",Float32.(u),start=[1,1,1],count=[-1,-1,1])
         NetCDF.putvar(ncv,"v",Float32.(v),start=[1,1,1],count=[-1,-1,1])
         NetCDF.putvar(ncη,"eta",Float32.(η),start=[1,1,1],count=[-1,-1,1])
-        #println("Initial conditions written to file.")
+
+        NetCDF.putvar(ncu,"t",Array(1:10))
+        #NetCDF.putvar(ncv,"t",Array(0:nout_total-1)*dtint*nout)
+        #NetCDF.putvar(ncη,"t",Array(0:nout_total-1)*dtint*nout)
+
+        # Attributes
+        Dictu = Dict{String,Any}("description"=>"Data from shallow-water model juls.")
+        Dictu["details"] = "Cartesian coordinates, f or beta-plane, Arakawa C-grid"
+        Dictu["reference"] = "github.com/milankl/juls"
+        Dictu["cfl"] = cfl
+        Dictu["g"] = gravity
+        Dictu["water_depth"] = water_depth
+        Dictu["bc_x"] = bc_x
+        Dictu["lbc"] = lbc
+        Dictu["c_D"] = c_D
+        Dictu["c_smag"] = c_smag
+        Dictu["initial_cond"] = initial_cond
+        Dictu["init_run_id"] = init_run_id
+        Dictu["phi"] = ϕ
+        Dictu["seamount_height"] = 150.
+        Dictu["Numtype"] = string(Numtype)
+        Dictu["output_dt"] = nout*dtint
+
+        NetCDF.putatt(ncu,"global",Dictu)
+        NetCDF.putatt(ncv,"global",Dictu)
+        NetCDF.putatt(ncη,"global",Dictu)
+
+        NetCDF.putatt(ncu,"t",Dict("units"=>"s","long_name"=>"time"))
+        NetCDF.putatt(ncv,"t",Dict("units"=>"s","long_name"=>"time"))
+        NetCDF.putatt(ncη,"t",Dict("units"=>"s","long_name"=>"time"))
+
+        NetCDF.putatt(ncu,"x",Dict("units"=>"m","long_name"=>"zonal coordinate"))
+        NetCDF.putatt(ncv,"x",Dict("units"=>"m","long_name"=>"zonal coordinate"))
+        NetCDF.putatt(ncη,"x",Dict("units"=>"m","long_name"=>"zonal coordinate"))
+
+        NetCDF.putatt(ncu,"y",Dict("units"=>"m","long_name"=>"meridional coordinate"))
+        NetCDF.putatt(ncv,"y",Dict("units"=>"m","long_name"=>"meridional coordinate"))
+        NetCDF.putatt(ncη,"y",Dict("units"=>"m","long_name"=>"meridional coordinate"))
+
+        NetCDF.putatt(ncu,"u",Dict("units"=>"m/s","long_name"=>"zonal velocity"))
+        NetCDF.putatt(ncv,"v",Dict("units"=>"m/s","long_name"=>"meridional velocity"))
+        NetCDF.putatt(ncη,"eta",Dict("units"=>"m","long_name"=>"sea surface height"))
 
         iout = 2    # counter for output time steps
 
@@ -39,6 +78,7 @@ function output_nc(ncs,u,v,η,i,iout)
     if i % nout == 0    # output only every nout time steps
         if output == 1
             NetCDF.putvar(ncs[1],"u",Float32.(u),start=[1,1,iout],count=[-1,-1,1])
+            #NetCDF.putvar(ncs[1],"t",[i*dtint],start=[iout],count=[1])
             NetCDF.putvar(ncs[2],"v",Float32.(v),start=[1,1,iout],count=[-1,-1,1])
             NetCDF.putvar(ncs[3],"eta",Float32.(η),start=[1,1,iout],count=[-1,-1,1])
             #println("Time step $iout written to file.")
