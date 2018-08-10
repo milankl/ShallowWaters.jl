2b88c69b9decf40e16e0b71625eafdf26c4ae5e2
milankl <kloewer@atmlxdt043.atm.ox.ac.uk>
2018-08-10 16:17:34 +0100

    inbounds added

--- src/ghost_points.jl
+++ src/ghost_points.jl
@@ -7,8 +7,8 @@ function add_halo(u,v,η)
     v = cat(2,zeros(Numtype,nvx+2*halo,halo),v,zeros(Numtype,nvx+2*halo,halo))
 
     # halo is always 1 for η
-    η = cat(1,zeros(Numtype,1,ny),η,zeros(Numtype,1,ny))
-    η = cat(2,zeros(Numtype,nx+2,1),η,zeros(Numtype,nx+2,1))
+    η = cat(1,zeros(Numtype,haloη,ny),η,zeros(Numtype,haloη,ny))
+    η = cat(2,zeros(Numtype,nx+2,haloη),η,zeros(Numtype,nx+2,haloη))
 
     ghost_points!(u,v,η)
     return u,v,η
@@ -23,75 +23,75 @@ function ghost_points_u_nonperiodic!(u)
     # u[end-1,:] = 0.
     # u[end,:] = 0.
 
-    @views u[:,1] .= one_minus_α*u[:,4]
-    @views u[:,2] .= one_minus_α*u[:,3]
-    @views u[:,end-1] .= one_minus_α*u[:,end-2]
-    @views u[:,end] .= one_minus_α*u[:,end-3]
+    @views @inbounds u[:,1] .= one_minus_α*u[:,4]
+    @views @inbounds u[:,2] .= one_minus_α*u[:,3]
+    @views @inbounds u[:,end-1] .= one_minus_α*u[:,end-2]
+    @views @inbounds u[:,end] .= one_minus_α*u[:,end-3]
 end
 
 function ghost_points_u_periodic!(u)
 
     # periodic bc
-    @views u[1,:] .= u[end-3,:]
-    @views u[2,:] .= u[end-2,:]
-    @views u[end-1,:] .= u[3,:]
-    @views u[end,:] .= u[4,:]
+    @views @inbounds u[1,:] .= u[end-3,:]
+    @views @inbounds u[2,:] .= u[end-2,:]
+    @views @inbounds u[end-1,:] .= u[3,:]
+    @views @inbounds u[end,:] .= u[4,:]
 
     # tangential bc
-    @views u[:,1] .= one_minus_α*u[:,4]
-    @views u[:,2] .= one_minus_α*u[:,3]
-    @views u[:,end-1] .= one_minus_α*u[:,end-2]
-    @views u[:,end] .= one_minus_α*u[:,end-3]
+    @views @inbounds u[:,1] .= one_minus_α*u[:,4]
+    @views @inbounds u[:,2] .= one_minus_α*u[:,3]
+    @views @inbounds u[:,end-1] .= one_minus_α*u[:,end-2]
+    @views @inbounds u[:,end] .= one_minus_α*u[:,end-3]
 end
 
 function ghost_points_v_nonperiodic!(v)
 
     # kinematic boundary condition
     # these values shouldn't change during a time step
-    # v[:,1] = zeero
-    # v[:,2] = zeero
-    # v[:,end-1] = zeero
-    # v[:,end] = zeero
+    v[:,1] .= zeero
+    v[:,2] .= zeero
+    v[:,end-1] .= zeero
+    v[:,end] .= zeero
 
-    @views v[1,:] .= one_minus_α*v[4,:]
-    @views v[2,:] .= one_minus_α*v[3,:]
-    @views v[end-1,:] .= one_minus_α*v[end-2,:]
-    @views v[end,:] .= one_minus_α*v[end-3,:]
+    @views @inbounds v[1,:] .= one_minus_α*v[4,:]
+    @views @inbounds v[2,:] .= one_minus_α*v[3,:]
+    @views @inbounds v[end-1,:] .= one_minus_α*v[end-2,:]
+    @views @inbounds v[end,:] .= one_minus_α*v[end-3,:]
 end
 
 function ghost_points_v_periodic!(v)
 
     # kinematic boundary condition
     # these values shouldn't change during a time step
-    # v[:,1] = zeero
-    # v[:,2] = zeero
-    # v[:,end-1] = zeero
-    # v[:,end] = zeero
+    v[:,1] .= 0.
+    v[:,2] .= 0.
+    v[:,end-1] .= 0.
+    v[:,end] .= 0.
 
-    @views v[1,:] .= v[end-3,:]
-    @views v[2,:] .= v[end-2,:]
-    @views v[end-1,:] .= v[3,:]
-    @views v[end,:] .= v[4,:]
+    @views @inbounds v[1,:] .= v[end-3,:]
+    @views @inbounds v[2,:] .= v[end-2,:]
+    @views @inbounds v[end-1,:] .= v[3,:]
+    @views @inbounds v[end,:] .= v[4,:]
 end
 
 function ghost_points_η_nonperiodic!(η)
 
     # corner points are copied twice, but it's faster!
-    @views η[1,:] .= η[2,:]
-    @views η[end,:] .= η[end-1,:]
+    @views @inbounds η[1,:] .= η[2,:]
+    @views @inbounds η[end,:] .= η[end-1,:]
 
-    @views η[:,1] .= η[:,2]
-    @views η[:,end] .= η[:,end-1]
+    @views @inbounds η[:,1] .= η[:,2]
+    @views @inbounds η[:,end] .= η[:,end-1]
 end
 
 function ghost_points_η_periodic!(η)
 
     # corner points are copied twice, but it's faster!
-    @views η[1,:] .= η[end-1,:]
-    @views η[end,:] .= η[2,:]
+    @views @inbounds η[1,:] .= η[end-1,:]
+    @views @inbounds η[end,:] .= η[2,:]
 
-    @views η[:,1] .= η[:,2]
-    @views η[:,end] .= η[:,end-1]
+    @views @inbounds η[:,1] .= η[:,2]
+    @views @inbounds η[:,end] .= η[:,end-1]
 end
 
 ## GATHER AND RENAME FUNCTIONS FOR CONVENIENCE
