1c2c4b97d1d4a876dabc5f9913b51780c42c68ab
milankl <milankloewer@gmx.de>
2018-10-09 15:52:03 +0100

    Function documentation

--- src/bottom_topography.jl
+++ src/bottom_topography.jl
@@ -1,5 +1,7 @@
+"""Returns a matrix of water depth for the whole domain that contains a
+Gaussian seamount in the middle. Water depth, heigth and width of the
+seamount are adjusted with the constants water_depth, topofeat_height and topofeat_width."""
 function seamount()
-    # A gaussian seamount
     xx_T,yy_T = meshgrid(x_T_halo,y_T_halo)
     bumpx = exp.(-((xx_T .- Lx/2).^2)/(2*topofeat_width^2))
     bumpy = exp.(-((yy_T .- Ly/2).^2)/(2*topofeat_width^2))
@@ -8,8 +10,10 @@ function seamount()
     return Numtype.(H)
 end
 
+"""Returns a matrix of water depth for the whole domain that contains a
+meridional Gaussian ridge in the middle. Water depth, heigth and width of the
+ridge are adjusted with the constants water_depth, topofeat_height and topofeat_width."""
 function ridge()
-    # A gaussian ridge
     xx_T,yy_T = meshgrid(x_T_halo,y_T_halo)
     bumpx = exp.(-((xx_T .- Lx/2).^2)/(2*topofeat_width^2))
 
@@ -17,8 +21,8 @@ function ridge()
     return Numtype.(H)
 end
 
+"""Returns a matrix of constant water depth specified by the constant water_depth."""
 function flat_bottom()
-    # A flat bottom
     H = fill(water_depth,(nx+2*haloη,ny+2*haloη))
     return Numtype.(H)
 end
