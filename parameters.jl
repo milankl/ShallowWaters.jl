aeface63f8bd6c601390f4cbb5f13aaced763b5a
milankl <milankloewer@gmx.de>
2018-10-12 12:52:00 +0100

    Function descriptions added

--- parameters.jl
+++ parameters.jl
@@ -10,10 +10,10 @@ const gravity = 10.             # gravitational acceleration
 const water_depth = 500.        # layer thickness at rest
 const ρ = 1e3                   # density
 
-const wind_forcing = "channel"
+const wind_forcing = "channel"  # "channel", "double_gyre", "shear" or "none"
 const Fx0 = 0.12                  # wind stress strength [Pa], default 0.12
 
-const topography_feature = "seamount"
+const topography_feature = "ridge"
 const topofeat_height = 150.      # height of seamount
 const topofeat_width = 100e3    # horizontal scale [m] of the seamount
 
@@ -23,7 +23,7 @@ const η_refh = 5.               # height difference [m] of the interface relaxa
 const η_refw = 50e3             # width [m] of the tangent used for the interface relaxation
 
 const cfl = 0.9                 # CFL number
-const Ndays = 50000               # number of days to integrate for
+const Ndays = 200               # number of days to integrate for
 
 # boundary condtions
 const bc_x = "periodic"         # "periodic" or anything else for nonperiodic
@@ -32,14 +32,14 @@ const lbc = 0.                  # lateral boundary condition parameter
 
 const adv_scheme = "ArakawaHsu" # "Sadourny" or "ArakawaHsu"
 
-const drag = 1e-5               # bottom drag coefficient
-const c_smag = 0.15             # Smagorinsky coefficient
+const drag = 1e-5               # bottom drag coefficient [dimensionless]
+const c_smag = 0.15             # Smagorinsky coefficient [dimensionless]
 
 const output = 1                # 1 for nc output 0 for none
-const output_dt = 3             # output time step in hours
-const outpath = "/local/kloewer/julsdata/"
+const output_dt = 6             # output time step in hours
+const outpath = "/network/aopp/cirrus/pred/kloewer/julsdata/"
 
-const initial_cond = "ncfile"     # "rest" or "ncfile"
+const initial_cond = "rest"   # "rest" or "ncfile"
 const init_run_id = 0           # only for starting from ncfile
 
 const ϕ = 30.                   # central latitue of the domain
--- src/gradients.jl
+++ src/gradients.jl
@@ -1,7 +1,6 @@
+"""Calculates the 2nd order centred gradient in x-direction on any grid (u,v,T or q).
+The size of dudx must be m-1,n compared to m,n = size(u)"""
 function ∂x!(dudx::AbstractMatrix,u::AbstractMatrix)
-    #= Calculates the gradient in x-direction on any grid (u,v,T or q).
-    The size of dudx must be m-1,n compared to m,n = size(u).=#
-
     m,n = size(dudx)
     @boundscheck (m+1,n) == size(u) || throw(BoundsError())
 
@@ -12,10 +11,9 @@ function ∂x!(dudx::AbstractMatrix,u::AbstractMatrix)
     end
 end
 
+"""Calculates the 2nd order centred gradient in y-direction on any grid (u,v,T or q).
+The size of dudy must be m,n-1 compared to m,n = size(u)."""
 function ∂y!(dudy::AbstractMatrix,u::AbstractMatrix)
-    #= Calculates the gradient in y-direction on any grid (u,v,T or q).
-    The size of dudy must be m,n-1 compared to m,n = size(u).=#
-
     m,n = size(dudy)
     @boundscheck (m,n+1) == size(u) || throw(BoundsError())
 
