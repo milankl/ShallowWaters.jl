7dee96a564875750d25be30d1f21b07802399562
Sarah Williamson <swilliamson@utexas.edu>
2024-04-10 16:17:23 -0500

    Verified that integration still works
    Matches the results from before changing the code

--- src/run_model.jl
+++ src/run_model.jl
@@ -41,3 +41,38 @@ function run_model(::Type{T},P::Parameter) where {T<:AbstractFloat}
     return S, P
 
 end
+
+
+#### Original ##################################################################################
+
+# In order to run the following you need to change lines in model_setup.jl
+
+function mk_run_model(::Type{T}=Float32;     # number format
+    kwargs...                            # all additional parameters
+    ) where {T<:AbstractFloat}
+
+    P = Parameter(T=T;kwargs...)
+    return mk_run_model(T,P)
+end
+
+function mk_run_model(P::Parameter)
+    @unpack T = P
+    return mk_run_model(T,P)
+end
+
+function mk_run_model(::Type{T},P::Parameter) where {T<:AbstractFloat}
+
+    @unpack Tprog = P
+
+    G = Grid{T,Tprog}(P)
+    C = Constants{T,Tprog}(P,G)
+    F = Forcing{T}(P,G)
+    S = ModelSetup{T,Tprog}(P,G,C,F)
+
+    Prog = initial_conditions(Tprog,S)
+    Diag = preallocate(T,Tprog,G)
+
+    Prog = time_integration(Prog,Diag,S)
+    return Prog
+
+end
